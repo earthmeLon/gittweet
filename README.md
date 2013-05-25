7fd66f2d7de6294124f8e93d0d68f67d19e8b19f
meLon <earthmeLon@gmail.com>
2013-05-25 13:02:27 -0500

    gittweet

---
+++ README.md
@@ -0,0 +1,28 @@
+#gittweet
+  [NeoRetro Group](https://neoretro.net)
+
+##Summary
+gittweet is a simple python script that will query Github for specific repositories and their current commit version.  If a new commit is detected, a Tweet is sent out according to settings within the script.
+
+
+##Requirements
+
+[ttytter](http://www.floodgap.com/software/ttytter/)
+
+  * Download the latest version available [here](http://www.floodgap.com/software/ttytter/dist2/)
+    * `sudo wget http://www.floodgap.com/software/ttytter/dist2/2.1.00.txt -O /usr/local/bin/ttytter`
+    * `sudo chmod +x /usr/local/bin/ttytter`
+  * Generate OAuth keyfile
+    * `ttytter -oauthwizard -keyf=KEYNAME`
+    * This will generate **./.ttytterkeyKEYNAME**
+
+##Configuration
+
+  * Adjust the location of ttytter binary if it is not found in */usr/local/bin*
+  * Tell the script where you would like to keep your state database file
+  * Change/Add repositories you wish to monitor and the ttytter keyfiles associated with the Twitter accounts you wish to use to send tweets
+
+  * Add gittweet.py to your crontab via `crontab -e`.  Remember, cron doesn't have access to shell variables, so don't use relative directories in the crontab task itself.
+
+    */5 * * * * /home/earthmeLon/src/gittweet/gittweet.py
+
---
+++ gittweet.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+""" Interface with GitHub
+https://api.github.com/repos/USER/REPO/commits
+
+NeoRetro Group
+"""
+
+import requests
+import json
+import sqlite3
+import subprocess
+
+ttytter = '/usr/bin/ttytter'
+statefile = '/home/earthmeLon/.gittweet.db'
+
+
+class github(object):
+
+    repos = []
+
+    def add(self, user, repo, keyfile):
+        #Check if the repo exists
+        rurl = 'https://api.github.com/repos/' + user + '/' + repo + '/commits'
+        if requests.get(rurl):
+            self.repos.append((repo, (rurl, keyfile)))
+            print "Added " + rurl
+        else:
+            print "Unable to add " + rurl
+
+    def poll(self):
+        try:
+            for repo in self.repos:
+                rurl = repo[1][0]
+                keyfile = repo[1][1]
+                r = requests.get(rurl)
+                if r.content:
+                    jsonx = json.loads(r.content)
+                    commit = jsonx[0]['commit']['tree']['sha']
+                    message = jsonx[0]['commit']['message']
+                    if len(message) > 140:
+                        message = message[:140]
+                print commit + ': ' + message
+                state.execute("SELECT * FROM state WHERE commitx=? LIMIT 1", (commit,))
+                if state.fetchall():
+                    print "Already seen commit: " + commit
+                else:
+                    state.execute("INSERT into state (commitx) VALUES (?)", (commit,))
+                    db.commit()
+                    subprocess.call([ttytter, '-keyf=' + keyfile, '-status=' + message])
+            #return (commit, message)
+        except Exception, e:
+            print repo
+            print e
+
+''' Configuration '''
+github = github()
+github.add('cjdelisle', 'cjdns', '/home/earthmeLon/.ttytterkeycjdns')
+#github.add('cjdelisle', 'cjdns', '/home/earthmeLon/.ttytterkeycjdns')
+#github.add('cjdelisle', 'cjdns', '/home/earthmeLon/.ttytterkeycjdns')
+
+''' Runtime '''
+try:
+    db = sqlite3.connect(statefile, check_same_thread=False)
+    state = db.cursor()
+    sql = 'create table if not exists "state" (id INTEGER PRIMARY KEY ASC AUTOINCREMENT, commitx BLOB)'
+    state.execute(sql)
+except Exception, e:
+    print "Unable to open state database: "
+    print e
+
+github.poll()
